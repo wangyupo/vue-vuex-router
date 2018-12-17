93903331e4ed4f5b94ad7463e5ca76d0de99c260
wangyupo <1106774262@qq.com>
2018-12-17 17:19:35 +0800

    常规提交

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "正在添加文件..."
+git add .
+echo -n "正在提交备注...，请填写备注（可空）"
+read remarks
+if [ ! -n "$remarks" ];then
+	remarks="常规提交"
+fi
+git commit -m "$remarks"
+echo "正在开始提交代码..."
+git push origin master
+echo "代码提交成功，正在关闭..."
