f7e18e9607e39364d8746d7570935538ae321841
wangyupo <1106774262@qq.com>
2018-12-21 11:50:56 +0800

    fastclick

--- src/main.js
+++ src/main.js
@@ -6,17 +6,22 @@ import store from './store'
 import i18n from './config/lang'
 import filters from './filters'
 import 'amfe-flexible'
-import lodash from 'lodash'
 
+// lodash函数库
+import lodash from 'lodash'
 Vue.prototype._ = lodash        // this._
 
-Vue.config.productionTip = false
+// 消除 click 移动浏览器300ms延迟
+import attachFastClick from 'fastclick'
+attachFastClick.attach(document.body)
 
 // 如果是非线上环境，加载 VConsole（移动端适用）
 // if (process.env.NODE_ENV !== 'production') {
 //     var VConsole = require('vconsole/dist/vconsole.min.js');
 //     var vConsole = new VConsole();
 // }
+
+Vue.config.productionTip = false
 
 // 全局过滤器
 Object.keys(filters).forEach(filterName => {
