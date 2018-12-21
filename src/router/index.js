696ca502d635e18a8d3dac625f6ef111508e9e80
wangyupo <1106774262@qq.com>
2018-12-21 18:58:04 +0800

    home

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // 正常加载
-// import Home from '../views/Home.vue'
+// import Home from '../views/home.vue'
 // import About from '../views/About.vue'
 
 // 按需（懒）加载（vue实现）
@@ -10,7 +10,7 @@ const Home = () => import( /* webpackChunkName: "home" */ '../views/home.vue')
 const About = () => import( /* webpackChunkName: "about" */ '../views/about.vue')
 
 // 按需（懒）加载（webpack实现）
-// const Home = r => require.ensure([], () => r(require('../views/Home.vue')), 'home')
+// const Home = r => require.ensure([], () => r(require('../views/home.vue')), 'home')
 // const About = r => require.ensure([], () => r(require('../views/About.vue')), 'about')
 
 Vue.use(Router)
