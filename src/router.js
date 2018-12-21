245951a6b06654833d0697e34680840d685f36e8
wangyupo <1106774262@qq.com>
2018-12-21 15:01:48 +0800

    router 按需加载

--- src/router.js
+++ src/router.js
@@ -1,17 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-// 正常加载
-// import Home from './views/Home.vue'
-// import About from './views/About.vue'
-
 // 按需加载（vue实现）
 const Home = () => import(/* webpackChunkName: "home" */ './views/Home.vue')
 const About = () => import(/* webpackChunkName: "about" */ './views/About.vue')
-
-// 按需加载（webpack实现）
-// const Home = r => require.ensure([], () => r(require('./views/Home.vue')), 'home')
-// const About = r => require.ensure([], () => r(require('./views/About.vue')), 'about')
 
 Vue.use(Router)
 
