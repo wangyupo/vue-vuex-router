a32a3953ada4d3d66249329f522a65e883611601
wangyupo <1106774262@qq.com>
2018-12-21 15:10:16 +0800

    vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // 正常加载
-// import Home from './views/Home.vue'
-// import About from './views/About.vue'
+// import Home from '../views/Home.vue'
+// import About from '../views/About.vue'
 
 // 按需（懒）加载（vue实现）
-const Home = () => import(/* webpackChunkName: "home" */ './views/Home.vue')
-const About = () => import(/* webpackChunkName: "about" */ './views/About.vue')
+const Home = () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+const About = () => import(/* webpackChunkName: "about" */ '../views/About.vue')
 
 // 按需（懒）加载（webpack实现）
-// const Home = r => require.ensure([], () => r(require('./views/Home.vue')), 'home')
-// const About = r => require.ensure([], () => r(require('./views/About.vue')), 'about')
+// const Home = r => require.ensure([], () => r(require('../views/Home.vue')), 'home')
+// const About = r => require.ensure([], () => r(require('../views/About.vue')), 'about')
 
 Vue.use(Router)
 
