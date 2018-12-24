41e98089faee45a3eaa3c42ea875663e7be77f04
wangyupo <1106774262@qq.com>
2018-12-24 13:49:37 +0800

    tabbar

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,23 +1,33 @@
 <template>
 	<div class="layout-container">
-        <slot></slot>
-    </div>
+		<div class="content-container">
+			<slot></slot>
+		</div>
+		<TabBar/>
+	</div>
 </template>
 
 <script>
+	import TabBar from "./tabBar";
+
 	export default {
-        name: "Layout",
+		name: "Layout",
+		components: {
+			TabBar
+		},
 		data() {
 			return {};
-		},
-		components: {}
+		}
 	};
 </script>
 
 <style lang="scss" scoped>
-    .layout-container {
-        height: 100vh;
-        overflow-x: hidden;
-        overflow-y: scroll;
-    }
+	.layout-container {
+		height: 100vh;
+		.content-container {
+			height: calc(100vh - 60px);
+			overflow-x: hidden;
+			overflow-y: scroll;
+		}
+	}
 </style>
---
+++ src/layouts/tabBar.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="tab-bar">
+		<div
+			v-for="(item, idx) in tabs"
+			:key="idx"
+			:class="['bar-item', activeIdx == idx ? 'active':'']"
+            @click="clickHandler(idx)"
+		>
+			<i :class="['iconfont', item.icon]"></i>
+			<div class="label">{{item.label}}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				tabs: [
+					{
+						label: "首页",
+						icon: "icon-weibo"
+					},
+					{
+						label: "我的",
+						icon: "icon-weibo"
+					}
+				],
+				activeIdx: "0"
+			};
+		},
+        methods: {
+            clickHandler(idx) {
+                this.activeIdx = idx;
+            },
+        }
+	};
+</script>
+
+<style lang="scss" scoped>
+	.tab-bar {
+		position: relative;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		height: 60px;
+		@include border-top();
+		.bar-item {
+            flex: 1;
+			font-size: 14px;
+			.iconfont {
+                font-size: 24px;
+			}
+		}
+        .bar-item.active {
+            color: red;
+        }
+	}
+</style>
