660b099cfb278c3f1d5af4512c91b34cdeadf2e2
wangyupo <1106774262@qq.com>
2018-12-27 11:31:36 +0800

    common scss

--- src/App.vue
+++ src/App.vue
@@ -17,22 +17,12 @@
 </script>
 
 <style lang="scss">
-	#app {
-		font-family: "Avenir", Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-	}
-
-	#nav {
-		padding: 30px;
-		a {
-			font-weight: bold;
-			color: #2c3e50;
-			&.router-link-exact-active {
-				color: #42b983;
-			}
-		}
-	}
+    @import "./style/reset.scss";
+    @import "./style/common.scss";
+    @import "~@/assets/iconfont/iconfont.css";
+    #app {
+        font-size: $text-common-size;
+        font-family: -apple-system, BlinkMacSystemFont, "PingFang SC",
+        "Helvetica Neue", STHeiti, "Microsoft Yahei", Tahoma, Simsun, sans-serif;
+    }
 </style>
