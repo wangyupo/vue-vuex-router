cf09adab925f7c782e504dbd19b3e4ab7245dd13
wangyupo <1106774262@qq.com>
2019-01-29 10:40:59 +0800

    filter

--- src/filters/index.js
+++ src/filters/index.js
@@ -76,10 +76,33 @@ const formatFloat = (val, pos = 2) => {
     return s;
 }
 
+/**
+ * 格式化时长
+ * @param second
+ * @returns {string}
+ */
+const realFormatSecond = (second) => {
+    let secondType = typeof second;
+
+    if (secondType === 'number' || secondType === 'string') {
+        second = parseInt(second);
+
+        let hours = Math.floor(second / 3600);
+        second = second - hours * 3600;
+        let mimute = Math.floor(second / 60);
+        second = second - mimute * 60;
+
+        return hours + ':' + ('0' + mimute).slice(-2) + ':' + ('0' + second).slice(-2)
+    } else {
+        return '0:00:00'
+    }
+};
+
 export default {
     timeFilter,
     formatPhone,
     formatBank,
     toThousands,
     formatFloat,
+    realFormatSecond,
 }
