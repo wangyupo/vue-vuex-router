ace6d751dd6183c7d7be243dc3111a28da854c38
wangyupo <1106774262@qq.com>
2019-01-31 10:49:24 +0800

    README.md

--- src/router/index.js
+++ src/router/index.js
@@ -50,6 +50,9 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
     // 做些什么，通常权限控制就在这里做哦
+
+    // 必须写next()哦，不然你的页面就会白白的，而且不报错，俗称"代码下毒"
+    next();
 });
 
 export default router;
