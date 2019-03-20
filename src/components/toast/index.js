a94a52456403008a3c40e3c21e392118d5e40ed0
wangyupo <1106774262@qq.com>
2019-03-20 18:33:50 +0800

    常规提交

--- src/components/toast/index.js
+++ src/components/toast/index.js
@@ -1,14 +1,25 @@
+/**
+ * Vue插件
+ * 参考链接：
+ * https://cn.vuejs.org/v2/api/#Vue-extend
+ * https://cn.vuejs.org/v2/api/#vm-mount
+ */
+
 import ToastComponent from './Toast'
 
 const Toast = {};
 
 Toast.install = (Vue) => {
+    // 第一步：使用基础 Vue 构造器，创建一个“子类”
     const ToastConstructor = Vue.extend(ToastComponent);
-    const instance = new ToastConstructor();                    // 创建toast子实例
-    instance.$mount(document.createElement('div'));             // 挂载实例到我们创建的DOM上
+    // 第二步：创造一个组件实例
+    const instance = new ToastConstructor();
+    // 第三步：将实例挂在到div上
+    instance.$mount(document.createElement('div'));
+    // 第四步：在body添加组件
     document.body.appendChild(instance.$el);
 
-    // 添加实例方法，以供全局调用
+    // 第五步：添加实例方法，以供全局调用
     Vue.prototype.$toast = (msg, type, duration = 2000) => {
         instance.message = msg;
         instance.type = type;
